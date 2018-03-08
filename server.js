5ee85e5e0299c21022e9a861531762d605c4acdf
Alina Rogalska (PGS Software) <arogalska@pgs-soft.com>
2018-03-08 15:41:00 +0100

    Tasks 3 and 4

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 const path = require('path');
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html', {root: path.join(__dirname, '/')});
@@ -12,6 +13,14 @@ app.get('/1/1', function (req, res) {
 
 app.get('/1/2', function (req, res) {
   res.sendFile('2.html', {root: path.join(__dirname, '/1/')});
+});
+
+app.get('/1/3', function (req, res) {
+  res.sendFile('3.html', {root: path.join(__dirname, '/1/')});
+});
+
+app.get('/1/4', function (req, res) {
+  res.sendFile('4.html', {root: path.join(__dirname, '/1/')});
 });
 
 app.listen(3000, function () {
