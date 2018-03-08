7c5d9c623c6a96bf08d52f6bf0507294b17bf4df
Alina Rogalska (PGS Software) <arogalska@pgs-soft.com>
2018-03-08 14:51:05 +0100

    task 2

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/1/1', function (req, res) {
   res.sendFile('1.html', {root: path.join(__dirname, '/1/')});
 });
 
+app.get('/1/2', function (req, res) {
+  res.sendFile('2.html', {root: path.join(__dirname, '/1/')});
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
